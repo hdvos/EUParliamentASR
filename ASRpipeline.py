7c209a2c4e5ff8422ace7cbb21aae9b99f6198db
hdvos <hdvos93@gmail.com>
2021-04-30 17:40:33 +0200

    weekend

--- ASRpipeline.py
+++ ASRpipeline.py
@@ -12,7 +12,7 @@ def prepare_output_folder(foldername):
 
 
 # Params #TODO: add as cmd line parameters
-STAGE = 1   # a parameter that enables skipping a step
+STAGE = 2   # a parameter that enables skipping a step
 
 SAVE_DIARIZATION_WAVFILES = True
 DIARIZATION_WAVFILES_LOCATION = '/home/hugo/MEGA/work/ASR/build_pipeline/diarization/test_wav_out'
@@ -29,8 +29,14 @@ def do_diarization(wavfile_path):
     diarization_bookeep = diarizer.split_wavfile(DIARIZATION_WAVFILES_LOCATION, 'test_bookkeep.json')
     pprint(asdict(diarization_results))
     pprint(asdict(diarization_bookeep))
+    return diarization_bookeep
 
 if __name__ == "__main__":
-    if STAGE <= 1:
+    if STAGE == 1:
         testwav = WAVFILE
-        do_diarization(testwav)
+        diarization_bookeep = do_diarization(testwav)
+
+    if STAGE <= 2:
+        try: 
+            diarization_bookkeep
+        except NameError

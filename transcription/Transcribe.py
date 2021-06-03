f018cbdadf8ef8f383c88e39a4d4c8068616ba60
hdvos <hdvos93@gmail.com>
2021-06-03 16:14:46 +0200

    bugfix

--- transcription/Transcribe.py
+++ transcription/Transcribe.py
@@ -64,18 +64,20 @@ model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
 
 
 def get_text(filename:str):
-    speech, rate = librosa.load(filename, sr=16000)
+    try:
+        speech, rate = librosa.load(filename, sr=16000)
 
-    input_values = tokenizer(speech, return_tensors = 'pt').input_values
-    #Store logits (non-normalized predictions)
-    logits = model(input_values).logits
+        input_values = tokenizer(speech, return_tensors = 'pt').input_values
+        #Store logits (non-normalized predictions)
+        logits = model(input_values).logits
 
-    #Store predicted id's
-    predicted_ids = torch.argmax(logits, dim =-1)
-    #decode the audio to generate text
-    transcription = tokenizer.decode(predicted_ids[0])
-    return transcription
-
+        #Store predicted id's
+        predicted_ids = torch.argmax(logits, dim =-1)
+        #decode the audio to generate text
+        transcription = tokenizer.decode(predicted_ids[0])
+        return transcription
+    except ValueError:
+        return ''
 
 def transcribe_wavfile(filename:str):
     if not os.path.exists(filename):
